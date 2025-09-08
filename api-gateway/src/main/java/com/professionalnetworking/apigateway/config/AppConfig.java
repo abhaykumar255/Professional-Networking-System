16070932a2386c1f403f66408c27a8ffc4564cb3
Abhay Kumar <rajputak255@gmail.com>
2025-09-08 21:06:01 +0530

    addding trace id and into the response header also

---
+++ api-gateway/src/main/java/com/professionalnetworking/apigateway/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.professionalnetworking.apigateway.config;
+
+//import feign.Capability;
+//import feign.micrometer.MicrometerCapability;
+//import io.micrometer.core.instrument.MeterRegistry;
+//import io.micrometer.tracing.Tracer;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class AppConfig {
+//
+//    @Bean
+//    public Capability capability(final MeterRegistry registry) {
+//        return new MicrometerCapability(registry);
+//    }
+//
+//}
---
+++ posts-service/src/main/java/com/professionalnetworking/postsservice/config/TraceIdFilter.java
@@ -0,0 +1,29 @@
+package com.professionalnetworking.postsservice.config;
+
+import io.micrometer.tracing.Tracer;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class TraceIdFilter implements Filter {
+
+    private final Tracer tracer;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        if (response instanceof HttpServletResponse httpResponse) {
+            String traceId = tracer.currentSpan() != null ?
+                    tracer.currentSpan().context().traceId() : "no-trace";
+            httpResponse.setHeader("X-Trace-ID", traceId);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
